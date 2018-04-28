67f441ac50d27c3bb70092ce95f3f419dcc00ebe
alexanderhlee <alexanderhlee@gmail.com>
2018-04-28 13:55:45 -0400

    Starting calculations

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,23 +13,52 @@ export class HomePage {
 
   numberOutput = {
     text: "0"
-  }
+  };
+
+  currentOp = -1;
+  runningTotal = 0;
 
   appenNum(num: string){
     if (this.numberOutput.text=="0")
     {
       this.numberOutput.text = num;
+      this.runningTotal = Number(num);
     }
     else
     {
-      let temp = this.numberOutput.text.concat(num);
-      this.numberOutput.text = temp;
+      if (this.currentOp == -1) {
+        let temp = this.numberOutput.text.concat(num);
+        this.numberOutput.text = temp;
+        this.runningTotal = Number(temp);
+      }
+      else{
+        if (this.currentOp == 1){
+          this.runningTotal += Number(this.numberOutput.text);
+        }
+        else if (this.currentOp == 0){
+          this.runningTotal -= Number(this.numberOutput.text);
+        }
+      }
+
     }
     
   }
 
   clearText(){
     this.numberOutput.text = "0";
+    this.runningTotal = 0;
+  }
+
+  setCurrentOpToAdd(){
+    this.currentOp = 1;
+  }
+
+  setCurrentOpToSub(){
+    this.currentOp = 0;
+  }
+
+  displayTotal(){
+    this.numberOutput.text = String(this.runningTotal);
   }
 
 }
